9243043d5ad3c51687c199f8c152f3584e88a5a1
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-11-23 01:14:49 +0800

    tambah link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
+<link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
 <body>
 <div class="bg-blue-400">
   <header class="sticky top-0 z-50">
