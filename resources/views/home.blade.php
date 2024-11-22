f4aa61dcb401a0f57f4a33ec490b61dfdc849dcd
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-11-23 02:06:41 +0800

    hehehe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/tailwind.css')
+  @vite('resources/css/app.css')
 </head>
 <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
 <body>
