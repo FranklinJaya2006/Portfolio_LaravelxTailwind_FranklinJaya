0dabf9ea017c2eb50d15323cb7e0aeac46bc3bd3
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-11-22 23:55:23 +0800

    coba deploy tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ export default {
       "./resources/**/*.blade.php",
       "./resources/**/*.js",
       "./resources/**/*.vue",
-      "./components/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
       extend: {},
