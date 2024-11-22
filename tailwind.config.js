9b814824ebe0173affa40b4850d341960792d227
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-11-23 01:52:58 +0800

    yah hijo sekali mi github ku

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+      "./resources/views/**/*.{js,ts,jsx,tsx}",
+      "./resources/js/**/*.{js,ts,jsx,tsx}",
+      "./resources/css/**/*.{js,ts,jsx,tsx}",
+      "./resources/sass/**/*.{js,ts,jsx,tsx}",
+      "./resources/tailwind.css",
       "./Components/**/*.{js,ts,jsx,tsx}",
-      "./src/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
       extend: {},
