d7fbf5f7c34f81d7e661d2b96fb78c971c1f49b3
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-11-23 01:33:58 +0800

    yahhh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./Components/**/*.{js,ts,jsx,tsx}"
+      "./Components/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
       extend: {},
