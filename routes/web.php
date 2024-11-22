19b57a908167b6c67f0eaa2524d458151c40886d
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2024-11-23 00:38:54 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\pindah;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/homeport', [pindah::class, 'pindahnav'])->name('homeport');
